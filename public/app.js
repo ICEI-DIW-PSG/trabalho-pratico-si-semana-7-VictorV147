6dfba1bba1f1896b364380a79e1ff217da72adad
Victor Viriato <victor.carmo.1478516@sga.pucminas.br>
2025-10-03 17:31:01 -0300

    Primeira parte js

---
+++ public/app.js
@@ -0,0 +1,38 @@
+const dados = [
+  {
+    id: 1,
+    titulo: "Nova Contratacao Chega na Toca da Raposa",
+    descricao:
+      "Novo reforco chega para reforcar o Cruzeiro para o restante da temporada 2025",
+    conteudo:
+      "Cruzeiro fecha contratacao de novo reforco colombiano, Sinisterra chega ao cruziero vindo do bourmenouth da inglaterra por 7 milhoes de euros. O ponta vem de recente lesao no joelho.",
+    categoria: "Mercado da bola",
+    autor: "Victor Viriato",
+    data: "2025-03-10",
+    imagem: "Sinisterra.jpeg",
+  },
+  {
+    id: 2,
+    titulo: "Provavel escalacao",
+    descricao:
+      "Provavel escalacao do Cruzeiro para confronto das quartas de finais da copa do brasil",
+    conteudo:
+      "Pre escalacao do cruzeiro para enfrentar o rival no confronto pelas quartas de finais da copa do cruzeiro na proxima quarta feira. Cruzeiro vem de uma vanatagem solida contruida no primeiro jogo que terminou em 2X0 para o maior de minas",
+    categoria: "Escalacao",
+    autor: "Victor Viriato",
+    data: "2025-03-10",
+    imagem: "campinho.jpeg",
+  },
+  {
+    id: 3,
+    titulo: "Zagueiro Convocado",
+    descricao: "Zagueiro titular do maior de minas é convocado pra selecao brasileira",
+    conteudo: "Fabricio Bruno aparece na lista de convocados de Lanceloti para respresentar a selecao nos proximos jogos da eliminatoria para a copa do mundo",
+    categoria: "Convocados",
+    autor: "Victor Viriato",
+    data: "2025-03-10",
+    imagem: "FBselecao.jpeg"
+  }
+];
+
+
